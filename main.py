bf920641328e03561c376c718dbcd6b6d5d6569c
AfpJesus <afpjesus00@gmail.com>
2021-12-16 17:33:25 +0000

    Flag and Color change

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import tkinter
 userwidth = 20
 rows = []
 brows = []
+flag = False
 
 class Cell():
     value = 0
     visible = False
+    flagged = False
 
     def __str__(self): 
         return str(self.value)
@@ -70,10 +72,10 @@ def CellLeftClicked(xy):
         for r in range(userwidth):
             for c in range(userwidth):
                 if rows[r][c].value == 'B':
-                    brows[r][c].config(text="   B   ")
+                    brows[r][c].config(text="   B   ", bg= "red")
                     rows[r][c].visible=True
                 else:
-                    brows[r][c].config(text="   " + str(rows[r][c].value) + "   ")
+                    brows[r][c].config(text="   " + str(rows[r][c].value) + "   ", bg = "white")
                     rows[r][c].visible=True
     elif rows[xy[0]][xy[1]].value == 0:
         CheckCell(xy[0], xy[1])
@@ -82,9 +84,28 @@ def CellLeftClicked(xy):
         rows[xy[0]][xy[1]].visible=True
         
 def CellRightClicked(xy):
-    if not rows[xy[0]][xy[1]].visible: 
-        brows[xy[0]][xy[1]].config(text= "   !   ")
+    if not rows[xy[0]][xy[1]].visible :
+        if rows[xy[0]][xy[1]].flagged == False:
+            brows[xy[0]][xy[1]].config(text= "   !   ", bg= "red")
+            rows[xy[0]][xy[1]].flagged = True
 
+        else:
+            brows[xy[0]][xy[1]].config(text= "       ", bg = "white")
+            rows[xy[0]][xy[1]].flagged = False
+
+def ClickSwitch(xy):
+    if flag:
+        CellRightClicked(xy)
+    else:
+        CellLeftClicked(xy)
+
+def Fswitch(fbutton):
+    global flag
+    flag = not flag
+    if flag:
+        fbutton.config(bg = "red")
+    else:
+        fbutton.config(bg = "white")
 
 def Window(userwidth):
     gameRows = GameStart(userwidth)
@@ -92,12 +113,14 @@ def Window(userwidth):
     window.geometry("800x775")
     window.maxsize(800, 775)
     # Code to add widgets will go here...
-    
+    fbutton = tkinter.Button(window,text= "Flag", bg = "white", command=lambda :Fswitch(fbutton))
+
+
     for brow in range(userwidth):
         bcols = []
         for bcol in range(userwidth):
             #bcols.append(tkinter.Button(window,text=str(gameRows[brow][bcol].value), command=lambda xy=[brow, bcol]: CellClicked(xy)))
-            bcols.append(tkinter.Button(window,text= "       ", command=lambda xy=[brow, bcol]: CellLeftClicked(xy)))
+            bcols.append(tkinter.Button(window,text= "       ", bg = "white", command=lambda xy=[brow, bcol]: ClickSwitch(xy)))
             #bcols.append(tkinter.Frame(window,width=30, height=25, background="gray"))
 
         brows.append(bcols)
@@ -108,6 +131,7 @@ def Window(userwidth):
             # brows[brow][bcol].bind("<Button-2>", CellRightClicked(xy))
             # brows[brow][bcol].bind("<Button-2>", CellRightClicked(xy))
 
+    fbutton.place(x=400,y=50)
     # for brow in range(userwidth):
     #     print(brows[brow])
     #     for bcol in range(userwidth):
