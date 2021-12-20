c7bc01d408a7d524fbaffa540e14385f6ef053f2
Rodrigo Ramos da Silva <a35479@alunos.ipb.pt>
2021-12-20 20:34:14 +0000

    Minesweeper_1.0

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import random
 import tkinter
 
-global userwidth
-global rows
-global brows
 userwidth = 20
 rows = []
 brows = []
 flag = False
 
+window = tkinter.Tk()
+window.geometry("800x775")
+window.maxsize(800, 775)
 class Cell():
     value = 0
     visible = False
@@ -23,22 +23,108 @@ class Cell():
 def CheckCell(x, y):
     if(rows[x][y].value == 0):
         for i in range(x-1,x+2):
-            if userwidth>i>=0:
-                for j in range(y-1,y+2):
-                    if (userwidth>j>=0 and rows[i][j].visible!=True):
-                        #print("About to check: " + str(i) + ", " + str(j))
-                        #print(str(i) + ", " + str(j) + ":" + str(rows[i][j]))
-                        brows[x][y].config(text="   "+str(rows[x][y].value)+"   ")
-                        rows[x][y].visible=True
-                        CheckCell(i,j)
+            for j in range(y-1,y+2):
+                print("About to check: " + str(i) + ", " + str(j))
+                if(((0<=i<20) and (0<=j<20) and rows[i][j].visible!=True)):
+                    print(str(i) + ", " + str(j) + ":" + str(rows[i][j]))
+                    brows[x][y].config(text="   "+str(rows[x][y].value)+"   ")
+                    rows[x][y].visible=True
+                    CheckCell(i,j)
     else:
         brows[x][y].config(text="   "+str(rows[x][y].value)+"   ")
         rows[x][y].visible=True
     
     
 
-def GameStart():
+def GameStart(window, userwidth):
+    global rows
+    global brows
+    global flag
+
+    brows= []
+    flag = False
     
+    MapInit(userwidth)
+
+    print(userwidth)
+    for brow in range(userwidth):
+        bcols = []
+        for bcol in range(userwidth):
+
+            #bcols.append(tkinter.Button(window,text=str(gameRows[brow][bcol].value), command=lambda xy=[brow, bcol]: CellClicked(xy)))
+            bcols.append(tkinter.Button(window,text= "       ", bg = "white", command=lambda xy=[brow, bcol]: ClickSwitch(xy)))
+            #bcols.append(tkinter.Frame(window,width=30, height=25, background="gray"))
+            
+        brows.append(bcols)
+
+        for bcol in range(userwidth):  
+            brows[brow][bcol].place(x=(100-(userwidth/2))+(brow*30), y=(200-(userwidth/2))+(bcol*25))
+            # xy=[brow, bcol]
+            # brows[brow][bcol].bind("<Button-1>", CellLeftClicked(xy))
+            # brows[brow][bcol].bind("<Button-2>", CellFlagClicked(xy))
+            # brows[brow][bcol].bind("<Button-2>", CellFlagClicked(xy))
+
+
+def CellLeftClicked(xy):
+    
+    print(str(xy[0]) + ", " + str(xy[1]) + ": " + str(rows[xy[0]][xy[1]]))
+    #HERE!!
+    
+    if rows[xy[0]][xy[1]].flagged:
+        return 0
+
+    if rows[xy[0]][xy[1]].value == 'B':
+        for r in range(userwidth):
+            for c in range(userwidth):
+                if rows[r][c].value == 'B':
+                    brows[r][c].config(text="   B   ", bg= "red")
+                    rows[r][c].visible=True
+                else:
+                    brows[r][c].config(text="   " + str(rows[r][c].value) + "   ", bg = "white")
+                    rows[r][c].visible=True
+    elif rows[xy[0]][xy[1]].value == 0:
+        CheckCell(xy[0], xy[1])
+    else:
+        brows[xy[0]][xy[1]].config(text= "   " + str(rows[xy[0]][xy[1]].value) + "   ")
+        rows[xy[0]][xy[1]].visible=True
+
+        
+def CellFlagClicked(xy):
+    if not rows[xy[0]][xy[1]].visible :
+        if rows[xy[0]][xy[1]].flagged == False:
+            brows[xy[0]][xy[1]].config(text= "   !   ", bg= "red")
+            rows[xy[0]][xy[1]].flagged = True
+
+        else:
+            brows[xy[0]][xy[1]].config(text= "       ", bg = "white")
+            rows[xy[0]][xy[1]].flagged = False
+
+def ClickSwitch(xy):
+    if flag:
+        CellFlagClicked(xy)
+    else:
+        CellLeftClicked(xy)
+
+def Fswitch(fbutton):
+    global flag
+    flag = not flag
+    if flag:
+        fbutton.config(bg = "red")
+    else:
+        fbutton.config(bg = "white")    
+
+def ResetGame(window,userwidth):
+    global brows
+    global rows
+    MapInit(userwidth)
+    for brow in range(userwidth):
+        for bcol in range(userwidth):
+            brows[brow][bcol].config(text = '       ', bg= "white")
+
+def MapInit(userwidth):
+    global rows
+    rows = []
+
     cell = Cell()
     for row in range(userwidth):
         col = []
@@ -64,86 +150,20 @@ def GameStart():
                 flag = False
     for r in rows:
         print(str(r))
-    
-    return rows
-
-def CellLeftClicked(xy):
-    
-    print(str(xy[0]) + ", " + str(xy[1]) + ": " + str(rows[xy[0]][xy[1]]))
-    #HERE!!
-    
-    if rows[xy[0]][xy[1]].value == 'B':
-        for r in range(userwidth):
-            for c in range(userwidth):
-                if rows[r][c].value == 'B':
-                    brows[r][c].config(text="   B   ", bg= "red")
-                    rows[r][c].visible=True
-                else:
-                    brows[r][c].config(text="   " + str(rows[r][c].value) + "   ", bg = "white")
-                    rows[r][c].visible=True
-    elif rows[xy[0]][xy[1]].value == 0:
-        CheckCell(xy[0], xy[1])
-    else:
-        brows[xy[0]][xy[1]].config(text= "   " + str(rows[xy[0]][xy[1]].value) + "   ")
-        rows[xy[0]][xy[1]].visible=True
-        
-def CellRightClicked(xy):
-    if not rows[xy[0]][xy[1]].visible :
-        if rows[xy[0]][xy[1]].flagged == False:
-            brows[xy[0]][xy[1]].config(text= "   !   ", bg= "red")
-            rows[xy[0]][xy[1]].flagged = True
-
-        else:
-            brows[xy[0]][xy[1]].config(text= "       ", bg = "white")
-            rows[xy[0]][xy[1]].flagged = False
-
-def ClickSwitch(xy):
-    if flag:
-        CellRightClicked(xy)
-    else:
-        CellLeftClicked(xy)
-
-def Fswitch(fbutton):
-    global flag
-    flag = not flag
-    if flag:
-        fbutton.config(bg = "red")
-    else:
-        fbutton.config(bg = "white")
 
 def Window(userwidth):
-    gameRows = GameStart(userwidth)
-    window = tkinter.Tk()
-    window.geometry("800x775")
-    window.maxsize(800, 775)
+    global window
+
     # Code to add widgets will go here...
     fbutton = tkinter.Button(window,text= "Flag", bg = "white", command=lambda :Fswitch(fbutton))
-
-
-    for brow in range(userwidth):
-        bcols = []
-        for bcol in range(userwidth):
-            #bcols.append(tkinter.Button(window,text=str(gameRows[brow][bcol].value), command=lambda xy=[brow, bcol]: CellClicked(xy)))
-            bcols.append(tkinter.Button(window,text= "       ", bg = "white", command=lambda xy=[brow, bcol]: ClickSwitch(xy)))
-            #bcols.append(tkinter.Frame(window,width=30, height=25, background="gray"))
-
-        brows.append(bcols)
-        for bcol in range(userwidth): 
-            label = tkinter.Label(brows[brow][bcol], text= "   " + str(rows[brow][bcol]) + "   ")
-            label.pack(padx=1, pady=1)
-            brows[brow][bcol].grid(row=brow, column=bcol)
-        #     brows[brow][bcol].grid(column=bcol, row= 0)
-
-            #brows[brow][bcol].place(x=(100-(userwidth/2))+(brow*30), y=(200-(userwidth/2))+(bcol*25))
-            # xy=[brow, bcol]
-            # brows[brow][bcol].bind("<Button-1>", CellLeftClicked(xy))
-            # brows[brow][bcol].bind("<Button-2>", CellRightClicked(xy))
-            # brows[brow][bcol].bind("<Button-2>", CellRightClicked(xy))
-
-    fbutton.place(x=400,y=50)
+    rbutton = tkinter.Button(window,text= "Reset", bg = "white", command=lambda :ResetGame(window,userwidth))
+    GameStart(window, userwidth)
+    #fbutton.place(x=400,y=50)
+    fbutton.pack()
+    rbutton.pack()
     # for brow in range(userwidth):
     #     print(brows[brow])
     #     for bcol in range(userwidth):
     window.mainloop()
 
-Window()
+Window(userwidth)
